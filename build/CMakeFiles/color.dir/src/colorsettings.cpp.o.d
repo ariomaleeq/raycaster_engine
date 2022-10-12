8bd468824d85c0c9c798141c03297d8d0b33ed55
ariomaleeq <maleeqario@gmail.com>
2022-10-12 13:03:46 +0200

    deleted build folder

--- build/CMakeFiles/color.dir/src/colorsettings.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/color.dir/src/colorsettings.cpp.o: \
- /home/ario/cpp/raycaster_engine/src/colorsettings.cpp \
- /usr/include/stdc-predef.h \
- /home/ario/cpp/raycaster_engine/include/colorsettings.h
--- build/CMakeFiles/rayrenderer.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for rayrenderer.
-# This may be replaced when dependencies are built.
